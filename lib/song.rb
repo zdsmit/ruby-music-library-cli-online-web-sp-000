2a68c1bdd35616bb951ee2fe0e9cc10677450311
Zach  Smith <zdsmith001@gmail.com>
2019-09-08 04:15:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -37,10 +37,16 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.select {|song| song.name == name}
+    @@all.detect {|song| song.name == name}
   end
 
-  def find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    if find_by_name(name)
+      find_by_name(name)
+    else
+      new_song = Song.create(name)
+      new_song
+    end
   end
 
 end
