42c1f3f015b8e90ea83c4fb7e8047fad2cd15095
Zach  Smith <zdsmith001@gmail.com>
2019-09-09 02:31:47 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,8 @@
 class MusicLibraryController
+  attr_reader :new_importer
 
   def initialize(path="./db/mp3s")
-    new_importer = MusicImporter.new(path)
+    @new_importer = MusicImporter.new(path)
     new_importer.import
   end
 
@@ -23,6 +24,55 @@ class MusicLibraryController
     end
   end
 
+  def list_songs
+    sorted_songs = Song.all.sort {|song_one, song_two| song_one.name <=> song_two.name}
+    sorted_songs.each_with_index do |song, index|
+      index += 1
+      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
 
+  def list_artists
+    sorted_artists = Artist.all.sort {|artist_one, artist_two| artist_one.name <=> artist_two.name}
+    sorted_artists.each_with_index do |artist, index|
+      index += 1
+      puts "#{index}. #{artist.name}"
+    end
+  end
+
+  def list_genres
+    sorted_genres = Genre.all.sort {|genre_one, genre_two| genre_one.name <=> genre_two.name}
+    sorted_genres.each_with_index do |genre, index|
+      index += 1
+      puts "#{index}. #{genre.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort {|song_one, song_two| song_one.name <=> song_two.name}.each_with_index do |song, index|
+        puts "#{index + 1}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort {|song_one, song_two| song_one.name <=> song_two.name}.each_with_index do |song, index|
+        puts "#{index + 1}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip
+    requested_song = list_songs.detect {|song| song.index == input}
+    split_song = Song.new_from_filename(requested_song)
+  end
 
 end
